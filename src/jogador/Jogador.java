4112a3043fee561c2d958f2faf448fc67826c4b4
DavidSilveira80 <davidpscontato@gmail.com>
2022-11-21 17:35:28 -0300

    Implementado construtor da classe Jogador.

--- src/jogador/Jogador.java
+++ src/jogador/Jogador.java
@@ -1,9 +1,15 @@
 package jogador;
 
 public class Jogador {
-    public String nome;
+    public String nome_jogador;
     public int gols;
     public int assistencias;
+
+    public Jogador(String nome_jogador, int gols, int assistencias){
+        this.nome_jogador = nome_jogador;
+        this.gols = gols;
+        this.assistencias = assistencias;
+    }
 
     public void adicionar_gol(int gol){
         this.gols += gol;
