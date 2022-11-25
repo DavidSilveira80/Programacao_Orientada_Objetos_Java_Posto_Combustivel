bc274ad13d6bf2c0dda3cdacccc95a9807192193
DavidSilveira80 <davidpscontato@gmail.com>
2022-11-25 17:33:08 -0300

    Adicionado enunciado do exercício Relógio.

---
+++ src/relogio/RelogioAplicacao.java
@@ -0,0 +1,34 @@
+package relogio;
+
+/*
+Crie as classes Relogio e Ponteiro e escreva um método main() para treinar a chamada aos
+métodos e atributos.
+Atributos da classe Relogio:
+
+  *ponteiroHora (tipo Ponteiro)
+  *ponteiroMinuto (tipo Ponteiro)
+  *ponteiroSegundo (tipo Ponteiro)
+Métodos da classe Relogio:
+
+acertarRelogio(int, int, int): Acerta o relógio, posicionando adequadamente
+cada ponteiro do relógio. Os parâmetros passados são hora, minuto e segundo.
+
+  *lerHora(): retorna a hora atual do relógio.
+  *lerMinuto(): retorna o minuto atual do relógio.
+  *lerSegundo(): retorna o segundo atual do relógio.
+Atributos da classe Ponteiro:
+
+ *posicao(int): indica em qual posição está o ponteiro (1, 2, 3, 4, etc.)
+
+Dica 1: Os ponteiros podem assumir as posições 1, 2, 3, 4 e assim por diante. Se um ponteiro
+dos minutos está na posição 3, significa que o método lerMinuto() deve retornar 15, que é o
+minuto correspondente a esta posição. O mesmo vale para o ponteiro dos segundos.
+
+Dica 2: No método acertarRelogio(), você deve passar a hora, minuto e segundo. Suponha
+que o horário fornecido seja 3 horas, 25 minutos e 50 segundos. O ponteiro das horas deve ser
+posicionado no 3; o dos minutos no 5; e o dos segundos no 10.
+
+ */
+
+public class RelogioAplicacao {
+}
