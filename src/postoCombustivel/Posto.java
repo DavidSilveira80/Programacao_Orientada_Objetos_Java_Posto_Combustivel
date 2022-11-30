89cbac6176d006cf283a7d7f51838b532fbf347b
DavidSilveira80 <davidpscontato@gmail.com>
2022-11-30 10:31:46 -0300

    Implementada geração de bomba de combustivel.

--- src/postoCombustivel/Posto.java
+++ src/postoCombustivel/Posto.java
@@ -4,85 +4,46 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Posto {
-    public static BombaCombustivel bomba1 = new BombaCombustivel(10000, 5.21, "Gasolina");
-
-    public static void gerarNota(double litros, double valor){
-        String produto = bomba1.getTipoCombustivel();
-        double precoUnitario = bomba1.getValorPorLitro();
-        System.out.println();
-        String descricao = String.format("%s:   R$ %.2f por Litro.", produto, precoUnitario);
-        String quantidade = String.format("%.2f litros  X R$ %.2f.", litros, precoUnitario);
-        System.out.println("POSTOS SILVEIRA");
-        System.out.println("Gerando Nota");
-        System.out.println(descricao);
-        System.out.println(quantidade);
-        System.out.printf("TOTAL: R$ %.2f\n", valor);
-        System.out.println();
-    }
-    public static void menuAbastecimento(){
-        Scanner entrada = new Scanner(System.in);
-        System.out.println();
-        System.out.println("Qual combustivel você quer?");
-        System.out.println("(G) -> Gasolina (D) -> Diesel (A) -> Álcool (E) -> Etanol");
-        System.out.print("Informe o tipo de combustivel: ");
-        char combustivel = entrada.next().charAt(0);
-        if(combustivel != 'G' && combustivel != 'D' && combustivel != 'A' && combustivel != 'E'){
-            System.out.println("Opção Inválida. Tente Novamente.");
-            menuAbastecimento();
-        }
-        bomba1.alterarTipoCombustivel(combustivel);
-        menuFormadeAbastecimento();
-
-    }
-    public static void menuFormadeAbastecimento(){
-        Scanner entrada = new Scanner(System.in);
-        System.out.println();
-        System.out.println("(V) -> Abastecer por Valor (L) -> Abastecer por Valor.");
-        System.out.print("Informe a forma de abastecimento: ");
-        char formaDeAbastecimento = entrada.next().charAt(0);
-        switch(formaDeAbastecimento){
-            case 'V':
-                System.out.println("Abastecer por Valor");
-                System.out.print("Entre Com o valor: R$ ");
-                double valorAbastecer = entrada.nextDouble();
-                double totalLitros = bomba1.abastecerPorValor(valorAbastecer);
-                gerarNota(totalLitros, valorAbastecer);
-                break;
-            case 'L':
-                System.out.println("Abastecer por Litro");
-                System.out.print("Entre com os Litros: ");
-                double abastecerLitros = entrada.nextDouble();
-                double valorTotalLitros = bomba1.abastecerPorLitro(abastecerLitros);
-                gerarNota(abastecerLitros, valorTotalLitros);
-                break;
-            default:
-                System.out.println("Opção Inválida. Tente Novamente");
-                menuFormadeAbastecimento();
-        }
-    }
 
     public static void main(String[] args) {
+        BombaCombustivel bombaPosto = new BombaCombustivel(0.0, 0.0, " ");
         Locale.setDefault(Locale.US);
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Bem-vindo ao posto Silveira");
-
-        int parada = 0;
-        while(parada == 0){
-            System.out.println("(A) -> Abastecer (N) -> Gerar Nota (S) -> Sair.");
-            System.out.print("O que deseja fazer: ");
-            char opcao = entrada.next().charAt(0);
-            switch (opcao){
-                case 'A':
-                    menuAbastecimento();
+        System.out.println("BEM-VINDO AO POSTO SILVEIRA");
+        int parada = 1;
+        while (parada == 1) {
+            System.out.println("1 - Gerar Bomba de Combustivel 4 - Sair");
+            System.out.print("Informe opção: ");
+            int opcao = entrada.nextInt();
+            switch (opcao) {
+                case 1:
+                    System.out.println("Gerar Bomba de Combustivel.");
+                    System.out.print("Informe o Estoque de Combustivel: ");
+                    double estoque = entrada.nextDouble();
+                    System.out.print("Informe o valor por Litro: R$ ");
+                    double valorPorLitro = entrada.nextDouble();
+                    System.out.print("Informe o tipo de Combustivel: (G) -> GASOLINA (D) -> DIESEL (A) -> ÁLCOOL (E) -> " +
+                            "ETANOL:  ");
+                    char tipoCombustivel = entrada.next().charAt(0);
+                    bombaPosto.alterarLitrosEstoque(estoque);
+                    bombaPosto.alterarValorPorLitro(valorPorLitro);
+                    bombaPosto.alterarTipoCombustivel(tipoCombustivel);
+                    System.out.println("GERANDO BOMBA DE COMBUSTIVEL.");
+                    System.out.println(bombaPosto.consultar());
                     break;
-                case 'S':
-                    System.out.println("Encerrando. Volte Sempre.");
-                    parada = 1;
+                case 4:
+                    System.out.println("ENCERRANDO. VOLTE SEMPRE.");
+                    parada = 2;
                     break;
                 default:
-                    System.out.println("Opção inválida. Tente novamente.");
+                    System.out.println("OPÇÃO INVÁLIDA. TENTE NOVAMENTE.");
             }
         }
     }
 }
+
+
+
+
+
